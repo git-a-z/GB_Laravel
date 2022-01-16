48708c2065aa85bb0a6b255080e07a24655360f1
a_z <azdvd@mail.ru>
2022-01-16 16:24:34 +0300

    GB_Laravel_lesson_02

---
+++ 00/routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\СategoryController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return '<h1 style="text-align:center;">Hello, world!</h1>';
+});
+
+Route::get('/01', function () {
+    return view('homework01');
+});
+
+Route::get('/02', function () {
+    return view('homework02');
+});
+
+Route::get('/home', [HomeController::class, 'index'])
+    ->name('home');
+
+Route::get('/category', [СategoryController::class, 'index'])
+    ->name('category::catalog');
+
+Route::get('/category/news/{id}', [СategoryController::class, 'news'])
+    ->where('id', '[0-9]+')
+    ->name('category::news');
+
+Route::get('/news', [NewsController::class, 'index'])
+    ->name('news::catalog');
+
+Route::get('/news/card/{id}', [NewsController::class, 'card'])
+    ->where('id', '[0-9]+')
+    ->name('news::card');
