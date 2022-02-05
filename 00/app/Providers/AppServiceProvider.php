2b4772cd0960c963b00bc2b961801051a828a7ce
a_z <azdvd@mail.ru>
2022-02-05 17:17:33 +0300

    GB_Laravel_lesson_07

--- 00/app/Providers/AppServiceProvider.php
+++ 00/app/Providers/AppServiceProvider.php
@@ -23,6 +23,56 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $menu = [
+            [
+                'title' => 'home',
+                'alias' => 'home',
+            ],
+            [
+                'title' => 'news',
+                'alias' => 'news::catalog',
+            ],
+            [
+                'title' => 'categories',
+                'alias' => 'category::catalog',
+            ],
+            [
+                'title' => 'admin',
+                'alias' => 'admin::news::catalog',
+            ],
+        ];
+        \View::share('menu', $menu);
+
+        $adminMenu = [
+            [
+                'title' => 'edit news',
+                'alias' => 'admin::news::catalog',
+            ],
+            [
+                'title' => 'add news',
+                'alias' => 'admin::news::new',
+            ],
+            [
+                'title' => 'edit categories',
+                'alias' => 'admin::category::catalog',
+            ],
+            [
+                'title' => 'add category',
+                'alias' => 'admin::category::new',
+            ],
+            [
+                'title' => 'news',
+                'alias' => 'news::catalog',
+            ],
+            [
+                'title' => 'categories',
+                'alias' => 'category::catalog',
+            ],
+        ];
+        \View::share('adminMenu', $adminMenu);
+
+        \View::share('noMoreNews', 'no more news');
+
+        \View::share('noMoreCategory', 'no more categories');
     }
 }
